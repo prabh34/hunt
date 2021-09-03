f3c0f95e8ff2f90a4057a1e03d9dbeda14f67008
prabh34 <77963863+prabh34@users.noreply.github.com>
2021-09-03 15:48:02 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "PARLIAMENT";
+const accessCode2 = "LEAP";
+const accessCode3 = "AMBUSH";
+
+function clues() {
+    
+    fill("grey")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("brown")
+    text("Hint: A GROUP OF OWL IS CALLED !!", 100,70)
+
+    fill("orange")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("blue")
+    text("Hint: A GROUP OF LEOPARDS IS CALLED !!", 700,170)
+
+    fill("pink")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("green")
+    text("Hint: A GROUP OF TIGERS IS CALLED !!", 100,270)
+
+}
